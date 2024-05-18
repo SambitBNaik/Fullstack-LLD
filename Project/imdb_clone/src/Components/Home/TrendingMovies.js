1119782323130e04cd94c54970d55c9e921eb216
Ronak Mund <ronakmund3@gmail.com>
2024-05-18 12:17:16 +0530

    feat(toggle): add toggle feature

--- Project/imdb_clone/src/Components/Home/TrendingMovies.js
+++ Project/imdb_clone/src/Components/Home/TrendingMovies.js
@@ -37,7 +37,7 @@
 //                 return(
 //                     <div
 //                     key={movie?.id}
-//                     className="w-[170px] h-[35vh] bg-center bg-cover rounded-xl m-4" 
+//                     className="w-[170px] h-[35vh] bg-center bg-cover rounded-xl m-4"
 //                     style={{
 //                         backgroundImage:`url(https://image.tmdb.org/t/p/original/t/p/w500/${movie?.poster_path})`,
 //                     }}
@@ -53,16 +53,25 @@
 
 // export default TrendingMovies
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { GetTrendingMovies } from "../../Service/GetTrendingMovies";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./TrendingMovie.css"
+import "./TrendingMovie.css";
+import { BsBookmarksFill } from "react-icons/bs";
 
 const TrendingMovies = () => {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [watchList, setWatchList] = useState([]);
+
+  useEffect(() => {
+    const watchListFromLocalStorage = localStorage?.getItem("movieWatchList");
+    if (watchListFromLocalStorage) {
+      setWatchList(JSON.parse(watchListFromLocalStorage));
+    }
+  }, []);
 
   useEffect(() => {
     GetTrendingMovies(page)
@@ -73,6 +82,32 @@ const TrendingMovies = () => {
         console.error(err);
       });
   }, [page]);
+
+  const toogleWatchlist = useCallback(
+    (movie) => {
+      const isMovieInWatchlist = watchList.some((item) => item.id === movie.id);
+      if (isMovieInWatchlist) {
+        setWatchList((prevMoviesList) => {
+          const filteredMovies =
+            prevMoviesList?.length > 0 &&
+            prevMoviesList.filter((m) => m.id !== movie.id);
+          localStorage.setItem(
+            "movieWatchList",
+            JSON.stringify(filteredMovies)
+          );
+          return filteredMovies;
+        });
+      } else {
+        setWatchList((prevMoviesList) => {
+          const newMoviesList =
+            prevMoviesList?.length > 0 ? [...prevMoviesList, movie] : [movie];
+          localStorage.setItem("movieWatchList", JSON.stringify(newMoviesList));
+          return newMoviesList;
+        });
+      }
+    },
+    [watchList]
+  );
 
   const settings = {
     dots: true,
@@ -88,17 +123,39 @@ const TrendingMovies = () => {
   const renderSlides = () => {
     if (!movies.length) return null;
 
-    return movies.map((movie, id) => (
-      <>
-        <div
-          key={movie?.id}
-          className="w-[170px] h-[35vh] bg-center bg-cover rounded-xl m-4"
-          style={{
-            backgroundImage:` url(https://image.tmdb.org/t/p/original/t/p/w500/${movie?.poster_path})`,
-          }}
-        ></div>
-      </>
-    ));
+    return movies.map((movie, id) => {
+      const isInWatchlist = watchList.some((item) => item.id === movie.id);
+      return (
+        <div className="">
+          <div
+            key={movie?.id}
+            className="w-[170px] h-[50vh] bg-center bg-cover rounded-xl m-8"
+            style={{
+              backgroundImage: ` url(https://image.tmdb.org/t/p/original/t/p/w500/${movie?.poster_path})`,
+            }}
+          >
+            {!isInWatchlist ? (
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toogleWatchlist(movie);
+                }}
+              >
+                <BsBookmarksFill style={{ color: "#e2e9e8" }} />
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  toogleWatchlist(movie);
+                }}
+              >
+                <BsBookmarksFill style={{ color: "#e9fa00" }} />
+              </button>
+            )}
+          </div>
+        </div>
+      );
+    });
   };
 
   return (
@@ -109,18 +166,13 @@ const TrendingMovies = () => {
           <div className="loader"></div>
         </div>
       ) : (
-        <Slider {...settings}> {renderSlides()}</Slider>
+        <Slider className={"slick-custom-class"} {...settings}>
+          {" "}
+          {renderSlides()}
+        </Slider>
       )}
     </div>
   );
 };
 
 export default TrendingMovies;
-
-
-
-
-
-
-
-
